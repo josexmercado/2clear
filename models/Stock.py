a8141779b7c6d80535bea651271df52fe106e080
migzs <34224337+migzs@users.noreply.github.com>
2018-01-19 15:48:38 +0800

    waduhek

---
+++ models/Stock.py
@@ -0,0 +1,26 @@
+from db import db
+
+class Stock(db.Model):
+    __tablename__ = 'stocks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Type = db.Column(db.String(45))
+    product = db.Column(db.String(45))
+    Amount = db.Column(db.Integer)
+    Date = db.Column(db.String(45))
+    
+
+    def __init__(self, *args, **kwargs):
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+                
+    def json(self):
+       return {'stockid':self.stockid}
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
